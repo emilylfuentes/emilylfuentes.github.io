7ebfa3d79fe3b4471c5a524c3971f59a43b29f99
Emily <emilylfuentes@gmail.com>
2018-06-19 18:11:43 -0400

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,7 @@
+alert("My first linked JavaScript!");
+
+<script src="/path/to/my/javascript_file.js"></script>
+
+alert("My first embedded JavaScript!");
+
+<a href="facebook.com"> javascript:alert("My first inline JavaScript!");</a>
